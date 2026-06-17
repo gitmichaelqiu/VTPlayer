35eec52bf5f27ea3ed8233d0d8072ea56cbadbb5
Michael Y. Qiu <altmickymike@outlook.com>
2026-06-17 22:02:38 +0900

    chore: initialize project template structure

---
+++ VTPlayer/VTPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VTPlayerApp.swift
+//  VTPlayer
+//
+//  Created by Michael Qiu on 6/17/26.
+//
+
+import SwiftUI
+
+@main
+struct VTPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
